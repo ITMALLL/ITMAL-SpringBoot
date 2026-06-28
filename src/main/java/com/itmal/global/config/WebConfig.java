5219f879ac4d6aac53bb3c73dab3dd56640ac830
cochae <ycw1203@naver.com>
2026-06-29 00:45:18 +0900

    ✨ Feat: header의 로고-메인 페이지 연결, footer 부가 기능 페이지 연결

--- src/main/java/com/itmal/global/config/WebConfig.java
+++ src/main/java/com/itmal/global/config/WebConfig.java
@@ -5,12 +5,21 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.time.Duration;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/policy/terms").setViewName("policy/terms");
+        registry.addViewController("/policy/privacy").setViewName("policy/privacy");
+        registry.addViewController("/policy/support").setViewName("policy/support");
+    }
+
     @Bean
     public WebClient webClient() {
         return WebClient.create();
