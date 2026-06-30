f3449f3b30ea78c4259518b6c13d6a5ed5615568
kanak71 <nanani715@gmail.com>
2026-06-30 14:02:44 +0900

    ✨ Feat: 마이페이지 구현

---
+++ src/main/java/com/itmal/mypage/controller/MypageApiController.java
@@ -0,0 +1,55 @@
+package com.itmal.mypage.controller;
+
+import com.itmal.answer.domain.Answer;
+import com.itmal.answer.service.AnswerService;
+import com.itmal.auth.domain.CustomUserDetails;
+import com.itmal.auth.domain.User;
+import com.itmal.auth.service.UserService;
+import com.itmal.comment.dto.CommentResponseDto;
+import com.itmal.comment.service.CommentService;
+import com.itmal.question.dto.QuestionDto;
+import com.itmal.question.service.QuestionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/mypage")
+public class MypageApiController {
+
+    private final QuestionService questionService;
+    private final AnswerService answerService;
+    private final CommentService commentService;
+    private final UserService userService;
+
+    @GetMapping("/questions")
+    public List<QuestionDto> getQuestion(@AuthenticationPrincipal CustomUserDetails userDetails,
+                                         @RequestParam(required = false) Long userId){
+        Long targetId = (userId != null) ? userId : userDetails.getUserId();
+        return questionService.findByUserId(targetId);
+    }
+
+    @GetMapping("/answers")
+    public List<Answer> getAnswer(@AuthenticationPrincipal CustomUserDetails userDetails,
+                                  @RequestParam(required = false) Long userId){
+        Long targetId = (userId != null) ? userId : userDetails.getUserId();
+        return answerService.getAnswerByQuestionId(userDetails.getUserId());
+    }
+
+    @GetMapping("/comment")
+    public List<CommentResponseDto> getMypageComment(@AuthenticationPrincipal CustomUserDetails userDetails,
+                                                     @RequestParam(required = false) Long userId){
+        Long targetId = (userId != null) ? userId : userDetails.getUserId();
+        return commentService.getMypageComment(userDetails.getUserId());
+    }
+
+
+
+
+}
